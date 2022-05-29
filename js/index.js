1f73c7e310b63aadaafde134c8b0fe43de26c94b
rosamcelheny <rmcelheny@gmail.com>
2022-05-29 13:21:04 -0400

    get about text functionality working

---
+++ js/index.js
@@ -0,0 +1,12 @@
+$("#about-button").click(function() {
+  $("#about").toggle();
+  console.log("clicked!");
+})
+
+$(".calendar").click(function() {
+  $("#about").hide();
+});
+
+$(".close-button").click(function() {
+  $("#about").hide();
+})
